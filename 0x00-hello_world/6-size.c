c2f00df7d3236a4c210a962ea168cb52ce90f255
Ayahkhattab <ayaahkhattab2017@gmail.com>
2023-04-20 20:07:41 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line using the size of various computer types
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	float f;
+printf("size of a char: %lu byte(s)\n",(unsigned long)sizeof(a));		
+return (0);
+}
