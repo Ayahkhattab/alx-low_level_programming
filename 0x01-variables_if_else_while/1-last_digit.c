55c3f03e91744cb62c06a96333afe9a3b3056ffe
Ayahkhattab <ayaahkhattab2017@gmail.com>
2023-04-21 19:35:44 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,27 +1,18 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
- * main - entry point
- *
- * Description: print the value of n status:
- *              greater than, is zero and is not lss than 6.
- *Return: 0 (Success)
-*/
+ *main-program entry point.
+
+ *Return:0 if no error, non zero if error.
+ **/
 int main(void)
 {
-	int n, digit;
+        char i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	digit = n % 10;
-
-	if (digit > 5)
-		printf("last digit of %i is %i and is greater than 5\n", n, digit);
-	else if (digit == 0)
-		printf("last digit of %i is %i and is 0\n", n, digit);
-	else if (digit < 6 && digit != 0)
-		printf("last digit of %i is %i and is less than 6 and not 0\n", n, digit);
-	return (0);
+        for (i = 'a'; i <= 'z'; i++)
+        {
+                putchar(i);
+        }
+        putchar('\n');
+        return (0);
 }
