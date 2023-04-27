df57f5f65c87c0fa438e812beb7024caec629b57
Ayahkhattab <ayaahkhattab2017@gmail.com>
2023-04-27 14:16:07 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+int _putchar(char c);
+
+int _putchar(int c);
+
+int _isdigit(int c);
+
+int mul(int a, int b);
+
+void printf_number(void);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+void print_line(int n);
+
+void print_diagona(int n);
+
+void print square(int size);
+
+void print_triangle(int size);
+
+void print_number(int n);
