674ff6cb0acd6e77d991ff6b05a23edff911d947
Ayahkhattab <ayaahkhattab2017@gmail.com>
2023-12-22 15:26:03 -0800

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,19 +9,27 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
-	unsigned long int i;
+    hash_table_t *ht;
+    unsigned long int i;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
-		return (NULL);
+    // Allocate memory for the hash table structure
+    ht = malloc(sizeof(hash_table_t));
+    if (ht == NULL)
+        return (NULL);
 
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
+    // Allocate memory for the array of linked lists
+    ht->array = malloc(sizeof(hash_node_t *) * size);
+    if (ht->array == NULL)
+    {
+        free(ht);  // Free the previously allocated memory for the hash table structure
+        return (NULL);
+    }
 
-	return (ht);
+    ht->size = size;
+
+    // Initialize each element in the array to NULL
+    for (i = 0; i < size; i++)
+        ht->array[i] = NULL;
+
+    return (ht);
 }
