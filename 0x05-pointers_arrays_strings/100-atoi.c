37b314ab317f01aa6f51ae6e90d408c95117fcea
Ayahkhattab <ayaahkhattab2017@gmail.com>
2023-12-22 15:28:54 -0800

    new updated

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,29 +1,42 @@
 #include "main.h"
 
 /**
- * _atoi - Entry point
- * Description: to check if letter is uppercase
- * @s: string to be converted
- * Return: no return
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ *
+ * Return: The converted integer.
  */
-int _atoi(char *s)
+int _atoi(const char *s)
 {
-	unsigned int i = 0;
-	int j;
-	int odd = 0;
+    unsigned int i = 0;
+    int j;
+    int sign = 1; // Default positive
 
-	for (j = 0; s[j]; j++)
-	{
-		if (s[j] >= '0' && s[j] <= '9')
-		{
-			i = i * 10 + s[j] - '0';
-		}
-		else if (s[j] == '-')
-			odd++;
-		else if (i > 0)
-			break;
-	}
-	if (odd % 2)
-		i = i * -1;
-	return (i);
+    for (j = 0; s[j]; j++)
+    {
+        if (s[j] >= '0' && s[j] <= '9')
+        {
+            i = i * 10 + s[j] - '0';
+        }
+        else if (s[j] == '-')
+        {
+            // Toggle the sign only if we haven't encountered any digits yet
+            if (i == 0)
+            {
+                sign *= -1;
+            }
+            else
+            {
+                // If we encounter '-' after digits, break the loop
+                break;
+            }
+        }
+        else if (i > 0)
+        {
+            // Break if we encounter a non-digit after parsing digits
+            break;
+        }
+    }
+
+    return (i * sign);
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _atoi(const char *s);
+
+#endif /* MAIN_H */
+
 int _putchar(char c);
 
 void reset_to_98(int *n);
